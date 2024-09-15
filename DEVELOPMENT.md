a87284db4f0eeeba771e1b8a694d6a0d7cb4af55
terwer <youweics@163.com>
2024-09-15 11:52:02 +0800

    feat: 优化 docker 部署脚本

---
+++ DEVELOPMENT.md
@@ -0,0 +1,17 @@
+## Dev
+
+```bash
+./startup.sh skipBuild
+```
+
+或者
+
+```bash
+./startup.sh
+```
+
+## Prod
+
+```bash
+./startup-rds.sh
+```
--- startup.sh
+++ startup.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
 
+# 构建项目
+echo "[1] 构建 java 项目 ...."
+# 判断是否跳过构建
+if [ "$1" != "skipBuild" ]; then
+    echo "正在执行构建流程..."
+    ./mvnw clean package -e -DskipTests
+    if [ $? -ne 0 ]; then
+        echo "构建失败，请检查错误日志。"
+        exit 1
+    fi
+    echo "[1] 构建完成"
+else
+    echo "跳过构建流程"
+fi
+echo "[1] Done. "
+
 # 创建网络
+echo "[2] 构建虚拟网络 ...."
 if ! docker network inspect jeepay-net >/dev/null 2>&1; then
-  echo "Network 'jeepay-net' not found. Creating it..."
+  echo "网络 'jeepay-net'不存在，开始创建 ..."
   docker network create jeepay-net
 else
-  echo "Network 'jeepay-net' already exists."
+  echo "网络 'jeepay-net' 已存在."
 fi
+echo "[2] Done. "
 
 # Now run Docker Compose
+echo "[3] 启动服务 ...."
 docker compose -f docker-compose-dev.yml up --build
+echo "[3] Done. "
 
 # 启动入口
 # 项目名称	方位地址
